ff538d97660771776fb2f9d7d50ce7e55f8d4a56
jinified <jinified@gmail.com>
2017-02-27 18:20:33 +0800

    Refactor some code

--- ass3/assignment3.py
+++ ass3/assignment3.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env pythonG
+#!/usr/bin/env python
 from __future__ import division
 import numpy as np
 import math
@@ -11,7 +11,7 @@ PREFIX = 'music_speech/'
 DENOM = 32768.0
 
 MFCC_MEAN_HEADERS = ["@ATTRIBUTE MFCC-{}_MEAN".format(i) for i in xrange(1, 27)]
-MFCC_STD_HEADERS = ["@ATTRIBUTE MFCC-{}_MEAN".format(i) for i in xrange(1, 27)]
+MFCC_STD_HEADERS = ["@ATTRIBUTE MFCC-{}_STD".format(i) for i in xrange(1, 27)]
 FINAL_HEADERS = ['@ATTRIBUTE class {music,speech}\n',
                  '@DATA']
 
@@ -29,7 +29,7 @@ def parseGroundTruth(path='music_speech.mf'):
 
 def readWav(path):
     """ Read wav file
-        returns: sampling frequency, normalized audio
+        Returns: sampling frequency, normalized audio
     """
     Fs, x = wavfile.read('{}{}'.format(PREFIX, path))
     return Fs, x / DENOM
@@ -40,22 +40,20 @@ def readWav(path):
 def mag(spectrum):
     """ Calculates magnitude of given FFT spectrum """
     return np.abs(spectrum)
-    # return np.sqrt(spectrum.real**2 + spectrum.imag**2)
 
 
 def preemphasis(signal, coeff=0.95):
     """ Performs preemphasis on signal
         coeff: preemphasis coefficient
     """
-    return np.append(signal[0], signal[1:]-coeff*signal[:-1])
+    preemp = np.append(signal[0], signal[1:]-coeff*signal[:-1])
+    return preemp
 
 
 # Main code
 
 def freq2mel(f):
-    """ Converts frequency to mel Scale which is a closer
-    representation of how human hear sound as it is not linear
-    """
+    """ Converts frequency to Mel Scale """
     return 1127.0 * np.log(1 + f / 700.0)
 
 
@@ -70,15 +68,12 @@ def getMelFilterBank(sampling_freq, lowfreq=0, highfreq=None, n_filters=26,
 
     # Use half of the sampling frequency as the upper bound
     highfreq = highfreq if highfreq else sampling_freq / 2
-    # buffer_size = sampling_freq
 
     lowmel = freq2mel(lowfreq)
     highmel = freq2mel(highfreq)
     melpoints = np.linspace(lowmel, highmel, n_filters + 2)
-    # print("Melpoints\n{}".format(melpoints))
     freqpoints = mel2freq(melpoints)
-    # print("Frequency points\n{}".format(freqpoints))
-    bin = (buffer_size+1) * freqpoints / sampling_freq
+    bin = buffer_size * freqpoints / sampling_freq
 
     fbank = np.zeros((n_filters, int(np.floor(buffer_size / 2 + 1))))
 
@@ -97,63 +92,59 @@ def getMelFilterBank(sampling_freq, lowfreq=0, highfreq=None, n_filters=26,
 
 
 def mfcc(spectrum, fbank):
-    """ Generates Mel filter bank from audio signal """
-
-    # Magnitude of spectrum
+    """ Generates MFCC vector for a given spectrum
+        fbank: Mel filter banks
+    """
     magnitude_spectrum = mag(spectrum)
 
     feats = np.dot(magnitude_spectrum, fbank.T)
     feats = np.log10(feats)
 
-    feature_vec = fftpack.dct(feats, type=2)
+    feature_vec = fftpack.dct(feats)
     return feature_vec
 
 
 def generateFFT(wav, buffer_size=1024, overlap=512,
                 window_func=np.hamming):
-    """ Divide wav file into individual buffer and apply fft to it
+    """ Splits given signal into frames and apply preemphasis to each frame
+    before applying FFT
         buffer_size: length of window
         overlap: step size
         window_func: windowing function applied to samples
-        Returns: positive frequency components
+        Returns: FFT frames
     """
     count = len(wav)
     splitted = np.array(
          # Generate only positive FFT components after multiplying samples
          # with designated window function
-         [np.fft.rfft(wav[i:i+buffer_size]*window_func(buffer_size))
+         [np.fft.rfft(preemphasis(wav[i:i+buffer_size])*window_func(buffer_size))
           for i in xrange(0, count, buffer_size - overlap)
           if count - i >= buffer_size])
     return splitted
 
 
 def calcMeanStd(features):
-    """ Returns mean and standard deviation of feature slice """
-    res = [np.mean(features), np.std(features)]
+    """ Returns mean and standard deviation of a feature slice """
+    # Rounds to sixth decimal point to pass the check
+    res = [np.round(np.mean(features), 6), np.round(np.std(features), 6)]
     return res
 
 
-def calcFeaturesMeanStd(gt):
-    """ Returns 1 X 26 vector for each file
-        gt: (path, category)
+def calcPerceptualFeatures(gt):
+    """ Returns 1 X 26 vector for a given audio signal
+        gt: (file path, category)
     """
 
     Fs, signal = readWav(gt[0])
-
-    # Apply Preemphasis filter
-    signal = preemphasis(signal)
-
     # Generate FFT frames after applying a Hamming Window
     frames = generateFFT(signal)
+    # Generates Mel Filter banks
     fbank = getMelFilterBank(Fs)
 
-    # Extracts MFCC features on each buffered FFT spectrum
-    feature_vec = np.apply_along_axis(
-        lambda frame: mfcc(frame, fbank), 1, frames)
-
+    # Extracts MFCC features from each buffered FFT spectrum
+    feature_vec = np.apply_along_axis(lambda frame: mfcc(frame, fbank), 1, frames)
     # Calculates mean and standard deviation for all extracted features
-    mfcc_vectors = (np.apply_along_axis(calcMeanStd,
-                    0, feature_vec)).flatten().tolist()
+    mfcc_vectors = (np.apply_along_axis(calcMeanStd, 0, feature_vec)).flatten().tolist()
 
     # Append ground truth category extracted from music_speech.mf
     mfcc_vectors.append(gt[1])
@@ -161,7 +152,7 @@ def calcFeaturesMeanStd(gt):
 
 
 def generateARFF(gt, arff_path='assignment3.arff'):
-    res = [','.join(map(str, calcFeaturesMeanStd(x))) for x in gt]
+    res = [','.join(map(str, calcPerceptualFeatures(x))) for x in gt]
     res = ARFF_HEADER + res
     with open(arff_path, 'w') as f:
         f.write('\n'.join(res))
@@ -169,21 +160,34 @@ def generateARFF(gt, arff_path='assignment3.arff'):
         f.write('\n')
 
 
-def plotMelFilters():
-    """ Use this to plot for whole frequency range """
-    fbank = getMelFilterBank(sampling_freq=22050, buffer_size=22050)
-    plt.plot(fbank.T)
-    plt.show()
-    # plt.savefig("MelFilters_full.png")
-    partial = fbank[:, :300]
-    plt.clf()
-    plt.plot(partial.T)
-    plt.show()
-    # plt.savefig("MelFilters_300HZ.png")
+# Plotting
+
+def plotMelFilters(n_filters=26, sampling_freq=22050, maxrange=12000,
+                   save_path=None):
+    """ Generate Mel filter bank plot
+        maxrange: maximum frequency range to be plotted
+        save_path: output plot destination
+    """
+    fbank = getMelFilterBank(sampling_freq)
+    highfreq = sampling_freq / 2
+    r, c = fbank.shape
+    frequencies = np.linspace(0, highfreq, c)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_title("{} Triangular MFCC filters {} Hz signal, window size {}"
+                 .format(n_filters, sampling_freq, 1024))
+    ax.set_xlabel("Frequency (Hz)")
+    ax.set_ylabel("Amplitude")
+    ax.set_xlim([0, maxrange])
+    for i in range(r):
+        ax.plot(frequencies, fbank[i])
+    if not save_path:
+        plt.show()
+    fig.savefig(save_path)
 
 
 if __name__ == '__main__':
     gt = parseGroundTruth()
-    # generateARFF(gt)
-    plotMelFilters()
-    # plotPartial()
+    generateARFF(gt, 'assignment3.arff')
+    plotMelFilters(save_path='MelFilters_Full.png')
+    plotMelFilters(maxrange=300, save_path='MelFilters_300HZ.png')
